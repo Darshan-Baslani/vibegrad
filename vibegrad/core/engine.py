Fixed Gradients not descenting issue

vibegrad/core/engine.py
@@ -115,9 +115,16 @@ class Tensor:
             # For matrix multiplication C = A @ B:
             # dL/dA = dL/dC @ B.T
             # dL/dB = A.T @ dL/dC
-            self.grad = np.matmul(out.grad, other.data.T)
-            other.grad = np.matmul(self.data.T, out.grad)
-        out.backward = _backward
+            if isinstance(self.grad, np.ndarray):
+                self.grad += np.matmul(out.grad, other.data.T)
+            else:
+                self.grad = np.matmul(out.grad, other.data.T)
+                
+            if isinstance(other.grad, np.ndarray):
+                other.grad += np.matmul(self.data.T, out.grad)
+            else:
+                other.grad = np.matmul(self.data.T, out.grad)
+        out._backward = _backward
         return out
 
     def __rmatmul__(self, other):

vibegrad/nn/_losses.py
@@ -27,19 +27,21 @@ class BCELoss:
 
         bce_loss = -(actual.data * np.log(pred_clipped) + (1-actual.data) * np.log(1-pred_clipped))
 
+        n_elements = np.prod(bce_loss.shape)
+        
         if self.reduction == "mean":
-            # bce_loss = bce_loss / pred_clipped.shape[0]
-            bce_loss = np.mean(bce_loss.data)
+            bce_loss = np.mean(bce_loss)
         elif self.reduction == 'sum':
-            bce_loss = np.sum(bce_loss.data)
+            bce_loss = np.sum(bce_loss)
         else:
             raise ValueError("Invalid reduction type. Use 'mean' or 'sum'")
 
         loss = Tensor(bce_loss, (pred, actual), "bce_loss") 
         def _backward():
             grad_pred = -(actual.data / pred_clipped - (1 - actual.data) / (1 - pred_clipped))
-            # pred.grad += grad_pred * loss.grad
-            pred.grad += grad_pred
+            if self.reduction == "mean":
+                grad_pred /= n_elements
+            pred.grad += grad_pred * loss.grad
         loss._backward = _backward
 
         return loss